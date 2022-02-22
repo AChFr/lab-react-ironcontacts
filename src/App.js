49bf1b89de16b4aa9537c77d10003b914dcfced2
AChFr <chirkovarseni@gmail.com>
2022-02-22 16:27:57 +0100

    se puede

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
+
 import './App.css';
+import contacts from "./contacts.json"
+import { useState } from "react"
+
+
 
 function App() {
+
+
+
+  let [shownList, setMovieList] = useState(contacts.splice(0, 5))
+
+
+  //let shownList = []
+
+  // contacts.forEach((elm, idx) => {
+  //   if (idx < 5) {
+  //     shownList.push(elm)
+  //   }
+  // })
+
+
+
+  const randomActor = () => {
+
+
+
+
+    const random = Math.floor(Math.random() * (contacts.length - shownList.length) + shownList.length)
+
+    let newList = [...shownList]
+    newList.push(contacts[random])
+
+    setMovieList(newList)
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className='App'>
+      <h1>IRONContacts</h1>
+      <button onClick={() => randomActor()}>ADD RANDOM CONTACT</button>
+      <table>
+        <tr>
+          <td><span>PICTURES</span></td>
+          <td><span>NAME</span></td>
+          <td><span>POPULARITY</span></td>
+          <td><span>WON OSCAR</span></td>
+          <td><span>WON EMMY</span></td>
+        </tr>
+        {shownList.map((elm, idx) => {
+
+
+
+          return (
+            <tr>
+              <td><img className="profilePic" src={elm.pictureUrl} alt={elm.name} /></td>
+              <td>{elm.name}</td>
+              <td>{elm.popularity}</td>
+              <td>{elm.wonOscar ? "🏆" : "no Oscar yet"}</td>
+              <td>{elm.wonEmmy ? "🏆" : "no Emmy yet"}</td>
+            </tr>
+          )
+
+        })}
+
+      </table>
+    </main>
   );
 }
 
